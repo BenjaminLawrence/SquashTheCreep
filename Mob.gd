771d42474777510cff9b44df4f95d463590efedc
BenjaminLawrence <benjamin13lawrence@gmail.com>
2024-11-28 14:01:37 +0800

    Add mob despawn

--- Mob.gd
+++ Mob.gd
@@ -13,3 +13,7 @@ func initialize(start_position, player_position):
 	var random_speed = randi_range(min_speed, max_speed)
 	velocity = Vector3.FORWARD * random_speed
 	velocity = velocity.rotated(Vector3.UP, rotation.y)
+
+
+func _on_visibility_notifier_screen_exited():
+	queue_free()
